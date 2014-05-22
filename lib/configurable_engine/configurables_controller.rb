f42f3bf6a32c1ff8ad48bb60791ee1a566544263
Michael Glass <me@mike.is>
2014-05-21 17:25:32 -0700

    admin controller can render errors

--- lib/configurable_engine/configurables_controller.rb
+++ lib/configurable_engine/configurables_controller.rb
@@ -5,12 +5,19 @@ module ConfigurableEngine
     end
 
     def update
-      Configurable.keys.each do |key|
-        configurable = Configurable.find_by_name(key) ||
-            Configurable.create {|c| c.name = key}
-        configurable.update_attribute(:value,params[key])
+      failures = Configurable
+        .keys.map do |key|
+          configurable = Configurable.find_by_name(key) ||
+              Configurable.create {|c| c.name = key}
+        end.reject do |configurable|
+          configurable.update_attributes(value: params[configurable.name])
+        end
+
+      if failures.empty?
+        redirect_to admin_configurable_path, :notice => "Changes successfully updated"
+      else
+        redirect_to admin_configurable_path, :alert => failures.flat_map(&:errors).flat_map(&:full_messages).join(',')
       end
-      redirect_to admin_configurable_path, :notice => "Changes successfully updated"
     end
   end
 end
