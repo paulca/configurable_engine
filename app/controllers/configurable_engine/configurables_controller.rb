b71268b224c65e5c99e6ce52f3faa7578668e21d
Michael Glass <me@mike.is>
2020-08-20 14:58:12 +0200

    can render most views with parent layout

--- app/controllers/configurable_engine/configurables_controller.rb
+++ app/controllers/configurable_engine/configurables_controller.rb
@@ -1,5 +1,7 @@
 module ConfigurableEngine
   class ConfigurablesController < ApplicationController
+    # this allows us to render url_helpers from parent app's layout
+    helper Rails.application.routes.url_helpers
     include ConfigurableEngine::ConfigurablesControllerMethods
   end
 end
