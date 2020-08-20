5eeedd00c716f0fb034ad1a31d5f2bdadd8f4dbb
Michael Glass <me@mike.is>
2020-08-20 15:06:01 +0200

    fix namespace problems

--- app/controllers/configurable_engine/configurables_controller.rb
+++ app/controllers/configurable_engine/configurables_controller.rb
@@ -2,6 +2,9 @@ module ConfigurableEngine
   class ConfigurablesController < ApplicationController
     # this allows us to render url_helpers from parent app's layout
     helper Rails.application.routes.url_helpers
+    # this ensures our local routes override any parent routes
+    helper ConfigurableEngine::Engine.routes.url_helpers
+
     include ConfigurableEngine::ConfigurablesControllerMethods
   end
 end
